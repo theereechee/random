71107413fae7817bbd50a097c3fe1321fbd351dd
Richard Antiabong <20932638+theereechee@users.noreply.github.com>
2023-03-04 22:08:42 -0600

    Uploading first finished task to github

---
+++ jpm_chase/initialize_seats_db.py
@@ -0,0 +1,19 @@
+import json
+import string
+
+
+SEATS_DB_FILE = "seats.json"
+NUM_ROWS = 20
+seats_db = {}
+
+
+def generate_plane_seats():
+    for row in string.ascii_uppercase[:NUM_ROWS]:
+        seats_db[row] = ["" for i in range(6)]
+
+    with open(SEATS_DB_FILE, "w") as f:
+        json.dump(seats_db, f, indent=4, sort_keys=True)
+
+
+if __name__ == "__main__":
+    generate_plane_seats()
---
+++ jpm_chase/reserve.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import os.path
+import json
+import argparse
+import initialize_seats_db
+
+seats_db_file = initialize_seats_db.SEATS_DB_FILE
+
+
+# Handle command line arguments.
+def parse_app_args():
+    ActionHelp = """
+        BOOK = Starts the daemon (default)
+        CANCEL = CANCEL reservation(s)
+        """
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="Process plane seat reservations",
+    )
+    parser.add_argument(
+        "action", choices=("BOOK", "CANCEL"), help=ActionHelp
+    )
+    parser.add_argument("starting_seat", help="Starting Seat Position e.g A1")
+    parser.add_argument("num_seats", type=int, help="Number of consecutive seats to book (or cancel), from starting seat")
+
+    return parser.parse_args()
+
+
+# Define functions
+def read_seats_db():
+    """Reads the seats database from file."""
+    if not os.path.isfile(seats_db_file):
+        initialize_seats_db.generate_plane_seats()
+
+    with open(seats_db_file, "r") as f:
+        seats_db = json.load(f)
+    return seats_db
+
+
+def write_reserved_seats(reserved_seats):
+    """Writes the list of reserved seats to file."""
+    with open(seats_db_file, "w") as f:
+        json.dump(reserved_seats, f, indent=4, sort_keys=True)
+
+
+def reserve_seats(starting_seat, num_seats):
+    """Reserves the given number of seats starting from the given seat."""
+    row, starting_seat_num = starting_seat[0], int(starting_seat[1:])
+
+    seats_reservation_state = read_seats_db()
+    row_seats = seats_reservation_state.get(row)
+
+    # Check if requested seat parameters are valid
+    if not valid_requests(row_seats, starting_seat_num, num_seats):
+        return False
+
+    # Fetch the requested seats.
+    requested_seats = row_seats[starting_seat_num : starting_seat_num + num_seats]
+
+    # Check if the requested seats are available
+    if "BOOKED" in requested_seats:
+        return False
+
+    # Reserve the requested seats
+    for i in range(starting_seat_num, starting_seat_num + num_seats):
+        seats_reservation_state[row][i] = "BOOKED"
+
+    write_reserved_seats(seats_reservation_state)
+    return True
+
+
+def cancel_seats(starting_seat, num_seats):
+    """Cancels the reservation of the given number of seats starting from the given seat."""
+    row, starting_seat_num = starting_seat[0], int(starting_seat[1:])
+
+    seats_reservation_state = read_seats_db()
+    row_seats = seats_reservation_state.get(row)
+
+    # Check if requested seat parameters are valid
+    if not valid_requests(row_seats, starting_seat_num, num_seats):
+        return False
+
+    # Cancel the reservation of the requested seats
+    for i in range(starting_seat_num, starting_seat_num + num_seats):
+        seats_reservation_state[row][i] = ""
+
+    write_reserved_seats(seats_reservation_state)
+    return True
+
+
+def valid_requests(row_seats, starting_seat_num, num_seats):
+    # Check if the starting seat number is valid.
+    if starting_seat_num not in list(range(6)):
+        return False
+
+    # Check if the requested row is available.
+    if not row_seats:
+        return False
+
+    # Check if the requested number of seats exceeds row capacity from starting seat.
+    if starting_seat_num + num_seats > 6:
+        return False
+
+    return True
+
+
+# Main program
+def main():
+    # Parse command-line arguments
+    args = parse_app_args()
+    if args.action == "BOOK":
+        success = reserve_seats(args.starting_seat, args.num_seats)
+    if args.action == "CANCEL":
+        success = cancel_seats(args.starting_seat, args.num_seats)
+
+    if success:
+        print("SUCCESS")
+    else:
+        print("FAIL")
+
+
+if __name__ == "__main__":
+    main()
